164c79d778e42f6574d9218623a830f24632d608
hyebin829 <smilerain779@gmail.com>
2022-06-02 14:19:50 +0900

    Add: streamerId column

--- server/models/fourth.js
+++ server/models/fourth.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
       category: { type: DataTypes.STRING(200), allowNull: false },
       title: { type: DataTypes.STRING(200), allowNull: false },
       startedAt: { type: DataTypes.STRING, allowNull: false },
+      streamerId: { type: DataTypes.STRING(20), allowNull: false },
     },
     { charset: "utf8mb4", collate: "utf8mb4_general_ci" }
   );
