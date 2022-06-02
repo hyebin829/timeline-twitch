4c4a3d6eeea791654d22bb7230168b38dc36a054
hyebin829 <smilerain779@gmail.com>
2022-06-02 23:59:05 +0900

    Feat : stream information page

--- front/src/routes/StreamInfo/index.tsx
+++ front/src/routes/StreamInfo/index.tsx
@@ -1,9 +1,13 @@
+import SelectDate from './Calendar'
+import FavoriteButton from './FavoriteButton'
 import Timeline from './Timeline'
 
 const StreamInfo = () => {
   return (
     <div>
       <h1> StreamInfo.</h1>
+      <FavoriteButton />
+      <SelectDate />
       <Timeline />
     </div>
   )
