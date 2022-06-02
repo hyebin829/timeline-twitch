afda16f59629a918e908eb017f1486191342bdea
hyebin829 <smilerain779@gmail.com>
2022-06-02 23:49:39 +0900

    Feat : get onAir

---
+++ server/routes/onairdate.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const { Fourth } = require("../models");
+const { Sequelize } = require("sequelize");
+const dayjs = require("dayjs");
+
+router.get("/:id", async (req, res) => {
+  console.log("adfadf");
+  try {
+    const dateArray = await Fourth.findAll({
+      where: { streamerId: req.params.id },
+      attributes: [
+        Sequelize.fn("DISTINCT", Sequelize.col("startedAt")),
+        "startedAt",
+      ],
+    });
+    const onAirDate = dateArray.map((item) =>
+      dayjs(item.dataValues.startedAt).format("YYYY-MM-DD")
+    );
+    res.status(200).json(onAirDate);
+  } catch (error) {}
+});
+
+module.exports = router;
