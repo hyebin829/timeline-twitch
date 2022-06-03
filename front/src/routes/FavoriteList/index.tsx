a86e8068fc5345c0d8bbd3df28bbec04a676ed30
hyebin829 <smilerain779@gmail.com>
2022-06-04 01:14:32 +0900

    Feat : favorite list

---
+++ front/src/routes/FavoriteList/index.tsx
@@ -0,0 +1,39 @@
+import { MouseEvent, useState } from 'react'
+import { Link } from 'react-router-dom'
+import dayjs from 'dayjs'
+import store from 'store'
+import styles from './favoriteList.module.scss'
+import { FiTrash2 } from 'react-icons/fi'
+
+const FavoriteList = () => {
+  const favoriteList = store.get('twitch-favorite') ?? []
+  const [list, setList] = useState(favoriteList)
+  const today = dayjs().format('YYYY-MM-DD')
+
+  const handleDelete = (e: MouseEvent<HTMLButtonElement>) => {
+    setList(
+      store.set(
+        'twitch-favorite',
+        favoriteList.filter((item: string) => item !== e.currentTarget.dataset.streamerId)
+      )
+    )
+  }
+
+  return (
+    <div className={styles.favoriteListWrapper}>
+      <h2>Favorite List</h2>
+      <ul className={styles.favoriteWrapper}>
+        {list.map((item: string) => (
+          <div key={item} className={styles.favorite}>
+            <Link to={`/streaminfo/${item}/${today}`}>{item}</Link>
+            <button type='button' onClick={handleDelete} data-streamer-id={item}>
+              <FiTrash2 size={20} />
+            </button>
+          </div>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default FavoriteList
