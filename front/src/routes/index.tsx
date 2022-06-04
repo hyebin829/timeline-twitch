0d1dd0e7a8d97857888703bb38abc75f7716cacd
hyebin829 <smilerain779@gmail.com>
2022-06-04 12:10:45 +0900

    Feat : notfound page

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -5,6 +5,7 @@ import StreamInfo from './StreamInfo'
 import FavoriteList from './FavoriteList'
 import styles from './routes.module.scss'
 import Layout from 'components/Layout'
+import NotFount from './NotFound'
 
 const App = () => {
   return (
@@ -15,7 +16,7 @@ const App = () => {
           <Route path='streaminfo' element={<StreamInfo />} />
           <Route path='streaminfo/:id/:date' element={<StreamInfo />} />
           <Route path='favorites' element={<FavoriteList />} />
-          <Route path='*' element={<div>404</div>} />
+          <Route path='*' element={<NotFount />} />
         </Route>
       </Routes>
     </div>
