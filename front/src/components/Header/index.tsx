3cd6c77774502e976ab68014eccff897e65dec60
hyebin829 <smilerain779@gmail.com>
2022-06-08 13:58:36 +0900

    Feat : add link to home in logo

--- front/src/components/Header/index.tsx
+++ front/src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import styles from './header.module.scss'
 import { IoLogoTwitch } from 'react-icons/io'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <h1>
-        Timeline
-        <IoLogoTwitch size={25} className={styles.logo} />
+        <Link to='/'>
+          Timeline
+          <IoLogoTwitch size={25} className={styles.logo} />
+        </Link>
       </h1>
     </header>
   )
