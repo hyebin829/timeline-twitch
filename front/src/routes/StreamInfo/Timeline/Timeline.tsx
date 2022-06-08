d709476d15847fcf0b23b34364eab05a7e8420c7
hyebin829 <smilerain779@gmail.com>
2022-06-08 14:30:12 +0900

    Delete : timeline state

--- front/src/routes/StreamInfo/Timeline/Timeline.tsx
+++ front/src/routes/StreamInfo/Timeline/Timeline.tsx
@@ -1,16 +1,15 @@
 import { useEffect } from 'react'
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
-import { useMount } from 'react-use'
 
 import { useRecoil } from 'hooks/state'
 import { getStream } from 'services/getStream'
-import { StreamerState, TimelineState } from 'states'
+import { StreamerState } from 'states'
+import { IStreamInfo } from 'types/index'
 
 import styles from './timeline.module.scss'
 
 const Timeline = () => {
-  const [timeline, setTimeline] = useRecoil(TimelineState)
   const [, setFavoriteStreamer] = useRecoil(StreamerState)
 
   const params = useParams()
@@ -21,37 +20,26 @@ const Timeline = () => {
     setFavoriteStreamer(streamerId)
   }, [setFavoriteStreamer, streamerId])
 
-  useMount(() => {
-    getStream(streamerId, selectedDate).then((res) => setTimeline(res.data))
-  })
-
   const { data } = useQuery(
     ['getStreaminfo', streamerId, selectedDate],
     () => getStream(streamerId, selectedDate).then((res) => res.data),
     {
       refetchOnWindowFocus: true,
       cacheTime: 30 * 60 * 1000,
-      onSuccess: () => {
-        setTimeline(data)
-      },
-      onError: () => {},
+      enabled: !!streamerId,
     }
   )
 
-  useEffect(() => {
-    setTimeline(data)
-  }, [setTimeline, data])
-
   return (
     <ol className={styles.timelineWrapper}>
-      {timeline?.length === 0 && (
+      {data?.length === 0 && (
         <div className={styles.noData}>
           자료가 없습니다. <br />
           스트리머 아이디 또는 날짜를 바르게 입력해주세요.
         </div>
       )}
-      {timeline &&
-        timeline.map((item) => (
+      {data &&
+        data.map((item: IStreamInfo) => (
           <li className={styles.timelineList} key={`${item.title}-${item.category}`}>
             <ul className={styles.streamInfo}>
               <li className={styles.category}>{item.category}</li>
