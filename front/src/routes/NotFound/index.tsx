f8a4dbd8c85b886175317b6e4ec03dc572cad1d9
hyebin829 <smilerain779@gmail.com>
2022-06-09 13:10:38 +0900

    Style : dark theme

--- front/src/routes/NotFound/index.tsx
+++ front/src/routes/NotFound/index.tsx
@@ -1,5 +1,7 @@
+import styles from './notFound.module.scss'
+
 const NotFount = () => {
-  return <div>페이지를 찾을 수 없습니다.</div>
+  return <div className={styles.notFound}>페이지를 찾을 수 없습니다.</div>
 }
 
 export default NotFount
