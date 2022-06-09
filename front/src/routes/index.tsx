a31227812fe384e660be8cad086a449443da7187
hyebin829 <smilerain779@gmail.com>
2022-06-09 11:46:06 +0900

    Feat : dark mode

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -6,8 +6,17 @@ import FavoriteList from './FavoriteList'
 import styles from './routes.module.scss'
 import Layout from 'components/Layout'
 import NotFount from './NotFound'
+import { useRecoil } from 'hooks/state'
+import { ThemeState } from 'states'
+import { useMount } from 'react-use'
 
 const App = () => {
+  const [theme] = useRecoil(ThemeState)
+
+  useMount(() => {
+    document.documentElement.setAttribute('timeline-theme', theme)
+  })
+
   return (
     <div className={styles.appWrapper}>
       <Routes>
