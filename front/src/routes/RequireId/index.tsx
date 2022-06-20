5b9272beb64c5de95378e8c611c32a6442a4bd36
hyebin829 <smilerain779@gmail.com>
2022-06-20 11:40:58 +0900

    Style : 문구 수정

--- front/src/routes/RequireId/index.tsx
+++ front/src/routes/RequireId/index.tsx
@@ -3,7 +3,7 @@ import styles from './requireId.module.scss'
 const RequireId = () => {
   return (
     <div className={styles.messageWrapper}>
-      아이디 입력이 필요합니다. <br />홈 화면에서 검색어를 입력해주세요.
+      스트리머 아이디 입력이 필요합니다. <br />홈 화면에서 스트리머 아이디를 검색해주세요.
     </div>
   )
 }
