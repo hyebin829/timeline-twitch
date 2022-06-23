ceb7542d80d1f8193ea0f12e6fcada2f1579ff57
hyebin829 <smilerain779@gmail.com>
2022-06-23 21:24:35 +0900

    mod

--- src/services/getStream.ts
+++ src/services/getStream.ts
@@ -3,6 +3,6 @@ import { axios } from 'hooks/worker'
 // const BASE_URL = '//ec2-43-200-124-60.ap-northeast-2.compute.amazonaws.com:3065'
 
 export const getStream = (streamerId: string | undefined, pickedDate: string | undefined) =>
-  axios.get(`/apiurl/api/streaminfo/${streamerId}/${pickedDate}`)
+  axios.get(`/api/api/streaminfo/${streamerId}/${pickedDate}`)
 
-export const getOnAirDay = (streamerId: string | undefined) => axios.get(`/apiurl/api/onairday/${streamerId}`)
+export const getOnAirDay = (streamerId: string | undefined) => axios.get(`/api/api/onairday/${streamerId}`)
