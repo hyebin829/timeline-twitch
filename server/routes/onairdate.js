f7362c43d5537c69e5b831b63bdd8e1987417887
hyebin829 <smilerain779@gmail.com>
2022-06-26 22:16:54 +0900

    Refactor : remove comment

--- server/routes/onairdate.js
+++ server/routes/onairdate.js
@@ -1,24 +1,18 @@
-const express = require("express");
-const router = express.Router();
-const { Fourth } = require("../models");
-const { Sequelize } = require("sequelize");
-const dayjs = require("dayjs");
+const express = require('express')
+const router = express.Router()
+const { Fourth } = require('../models')
+const { Sequelize } = require('sequelize')
+const dayjs = require('dayjs')
 
-router.get("/:id", async (req, res) => {
-  console.log("adfadf");
+router.get('/:id', async (req, res) => {
   try {
     const dateArray = await Fourth.findAll({
       where: { streamerId: req.params.id },
-      attributes: [
-        Sequelize.fn("DISTINCT", Sequelize.col("startedAt")),
-        "startedAt",
-      ],
-    });
-    const onAirDate = dateArray.map((item) =>
-      dayjs(item.dataValues.startedAt).format("YYYY-MM-DD")
-    );
-    res.status(200).json(onAirDate);
+      attributes: [Sequelize.fn('DISTINCT', Sequelize.col('startedAt')), 'startedAt'],
+    })
+    const onAirDate = dateArray.map((item) => dayjs(item.dataValues.startedAt).format('YYYY-MM-DD'))
+    res.status(200).json(onAirDate)
   } catch (error) {}
-});
+})
 
-module.exports = router;
+module.exports = router
